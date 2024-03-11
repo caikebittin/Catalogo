07162c480408354d7ba0803ed0eb31f74d583dad
Caike Bittin <ikee.bittin@gmail.com>
2024-03-11 16:57:18 -0300

    Add project files.

---
+++ Catalogo.Api/Context/CatalagoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalogo.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Api.Context;
+
+public class CatalagoDbContext : DbContext
+{
+    public CatalagoDbContext(DbContextOptions<CatalagoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+}
---
+++ Catalogo.Api/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Principal;
+
+namespace Catalogo.Api.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
