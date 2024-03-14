8ef5ca403621f27ee7e305aadae6e1bedcf15949
Caike Bittin <ikee.bittin@gmail.com>
2024-03-14 18:02:46 -0300

    inserido função delete e put

--- Catalogo.Api/Controllers/ProdutosController.cs
+++ Catalogo.Api/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using Catalogo.Api.Context;
 using Catalogo.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalogo.Api.Controllers;
 
@@ -21,23 +22,57 @@ public class ProdutosController : ControllerBase
         var produtos = _context.Produtos.ToList();
 
         if (produtos is null)
-        {
             return NotFound("Produtos não encontrados.");
-        }
 
         return produtos;
     }
 
-    [HttpGet("{id:int}")]
+    [HttpGet("{id:int}", Name = "ObterProduto")]
     public ActionResult<Produto> Get(int id)
     {
         var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
 
         if (produto is null)
-        {
             return NotFound("Produto não encontrado.");
-        }
 
         return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.ProdutoId)
+            return BadRequest();
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto is null)
+            return NotFound("Produto não encontrado");
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok(produto);
     }
 }
